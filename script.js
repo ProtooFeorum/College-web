dbcaeae5c07014a1d55d25dc548e668050229bf8
ProtooFeorum <protoofeorum@gmail.com>
2021-11-19 00:28:19 +0300

    Сворачивающаяся шапка

---
+++ script.js
@@ -0,0 +1,34 @@
+// let userName = "Fiodor";
+// let age = 15.5;
+// let boo = true;
+
+// const header = document.querySelector("#header");
+// const navLinks = document.querySelectorAll(".navLink")
+
+// console.log(navLinks);
+
+// function sayHello() {
+//     let massage = "Hello " + userName;
+//     console.log(massage);
+// }
+
+// sayHello();
+
+// function simpleMath(a, b) {
+//     let result = a+b;
+//     return result;
+// }
+
+// let sum = simpleMath(10, 23);
+// console.log(sum);
+
+window.addEventListener("scroll", function(){
+    let scrollPos = this.window.scrollY;
+    if(scrollPos > 199){
+   header.classList.add('red');
+ } else{
+   header.classList.remove('red');  
+ }
+
+  console.log(scrollPos);
+});
